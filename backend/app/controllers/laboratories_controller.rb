3fdc3df1ceb0a947b250f2e68ace1d740e3496dc
mashumarrow <134787738+mashumarrow@users.noreply.github.com>
2026-07-10 21:23:59 +0900

    WIP before switching branch

--- backend/app/controllers/laboratories_controller.rb
+++ backend/app/controllers/laboratories_controller.rb
@@ -1,6 +1,13 @@
 class LaboratoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_completed_profile!
 
   def show
+  end
+
+  private
+
+  def require_completed_profile!
+    redirect_to setup_path unless current_user.profile_complete?
   end
 end
